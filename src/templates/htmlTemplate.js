8f2c63862f1f39a6c5aa660e470ec62f4cf7ca01
Ben Freudman <bfreud94@gmail.com>
2021-02-25 22:52:22 -0500

    Refactored html template

--- src/templates/htmlTemplate.js
+++ src/templates/htmlTemplate.js
@@ -1,55 +1,32 @@
-const getHeaders = (headers) => {
-    let headersHTML = '';
-    headers.map((header) => {
-        headersHTML += `<th>${header}</th>`
-    });
-    return headersHTML;
-}
+import styles from './styles';
 
 const getRows = (values) => {
     let rowsHTML = '';
     values.map((value) => {
-        rowsHTML += `<th>${value}</th>`
+        rowsHTML += `<th class='tableRow'>${value === 'PLUSMINUS' ? '+/-' : value}</th>`
     });
     return rowsHTML;
-}
+};
+
+const metaTags = () => (
+    `<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />`
+);
 
 export default (headers, values, playerName) => {
-    const headersHTML = getHeaders(headers);
+    const headersHTML = getRows(headers);
     const rowsHTML = getRows(values);
     return `<!DOCTYPE html>
         <html lang="en">
             <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                ${metaTags()}
                 <style>
-                    body {
-                        font-family: "Poppins", Arial, Helvetica, sans-serif;
-                        background: rgb(22, 22, 22);
-                        color: #fff;
-                        max-width: 300px;
-                    }
-                    .app {
-                        max-width: 300px;
-                        padding: 20px;
-                        flex-direction: row;
-                        border-top: 3px solid rgb(16, 180, 209);
-                        background: rgb(31, 31, 31);
-                        align-items: center;
-                    }
-                    img {
-                        width: 50px;
-                        height: 50px;
-                        margin-right: 20px;
-                        border-radius: 50%;
-                        border: 1px solid #fff;
-                        padding: 5px;
-                    }
+                    ${styles()}
                 </style>
             </head>
             <body>
-                <div class="app">
+                <div class='app'>
                 <h4>${playerName}</h4>
                 <table>
                     <tr>
---
+++ src/templates/styles.js
@@ -0,0 +1,20 @@
+export default () => (
+    `body {
+        font-family: "Poppins", Arial, Helvetica, sans-serif;
+        background: rgb(22, 22, 22);
+        color: #fff;
+        max-width: 500px;
+    }
+    .app {
+        max-width: 100%;
+        padding: 20px;
+        flex-direction: row;
+        border-top: 3px solid rgb(16, 180, 209);
+        background: rgb(31, 31, 31);
+        align-items: center;
+    }
+    .tableRow {
+        padding: 0px 30px 0px 0px;
+        text-align: left;
+    }`
+);
